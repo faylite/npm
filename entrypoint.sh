861a6d69c52e6f083605b840ea90c2105940c7eb
Benjamin Vevang <benvev95@gmail.com>
2019-05-23 16:44:23 +0200

    Concatenate to config file instead of overriding

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,7 +19,7 @@ if [ -n "$NPM_AUTH_TOKEN" ]; then
   fi
 
   # Allow registry.npmjs.org to be overridden with an environment variable
-  printf "//%s/:_authToken=%s\\nregistry=%s\\nstrict-ssl=%s" "$NPM_REGISTRY_URL" "$NPM_AUTH_TOKEN" "${NPM_REGISTRY_SCHEME}://$NPM_REGISTRY_URL" "${NPM_STRICT_SSL}" > "$NPM_CONFIG_USERCONFIG"
+  printf "//%s/:_authToken=%s\\nregistry=%s\\nstrict-ssl=%s" "$NPM_REGISTRY_URL" "$NPM_AUTH_TOKEN" "${NPM_REGISTRY_SCHEME}://$NPM_REGISTRY_URL" "${NPM_STRICT_SSL}" >> "$NPM_CONFIG_USERCONFIG"
 
   chmod 0600 "$NPM_CONFIG_USERCONFIG"
 fi
